001021b53cafbc27bad2b76a82b2f3284472b659
Jalal Eid <jalaleid.136@gmail.com>
2024-02-07 02:52:29 +0300

    simple edit

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const titleMedium =
+      TextStyle(fontSize: 18, fontWeight: FontWeight.w600);
+}
--- lib/featuers/home/presentation/widgets/home_view_body.dart
+++ lib/featuers/home/presentation/widgets/home_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
-import 'featured_lits_view_item.dart';
+import 'featured_books_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -9,26 +9,5 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(children: [CustomAppBar(), FeaturedBooksListView()]);
-  }
-}
-
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            child: FeaturedListViewItem(),
-          );
-        },
-      ),
-    );
   }
 }
