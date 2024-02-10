18bc21f19112a9fce4681afc2b8d2cc0ce564db9
Jalal Eid <jalaleid.136@gmail.com>
2024-02-10 12:31:37 +0300

    refactor to use case class

---
+++ lib/core/use_case/use_case.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+
+import '../errors/failure.dart';
+
+abstract class UseCase<Type, Param> {
+  Future<Either<Failure, Type>> call([Param param]);
+}
--- lib/featuers/home/domain/use_cases/fetch_featuerd_books_use_case.dart
+++ lib/featuers/home/domain/use_cases/fetch_featuerd_books_use_case.dart
@@ -4,13 +4,14 @@ import 'package:dartz/dartz.dart';
 import '../../../../core/errors/failure.dart';
 import '../entities/book_entity.dart';
 
-class FetchFeaturedBooksCase {
+class FetchFeaturedBooksCase extends UseCase<List<BookEntity>, NoParam> {
   final HomeRepo homeRepo;
 
-  FetchFeaturedBooksCase({required this.homeRepo});
-
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
-    // check permssion
-    return homeRepo.fetchFeaturedBooks();
+  FetchFeaturedBooksCase(this.homeRepo);
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async {
+    return await homeRepo.fetchFeaturedBooks();
   }
 }
+
+class NoParam {}
